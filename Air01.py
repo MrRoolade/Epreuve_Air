228bbd6d11f1e6ab392759732ad310d13a0e82f2
MrRoolade <romain.recasens@free.fr>
2023-07-19 07:47:54 +0200

    Split en fonction

---
+++ Air01.py
@@ -0,0 +1,54 @@
+#~~> MrRøølåÐe <~~#
+## Split en fonction
+### un programme qui découpe une chaîne de caractères en tableau en fonction du séparateur donné en 2e argument.    
+##### Afficher error et quitter le programme en cas de problèmes d’arguments.
+
+import sys
+
+# Parsing
+user_value = sys.argv[1]
+number_of_value = len(sys.argv)
+separator=sys.argv[-1]
+
+# Gestion des erreurs 
+def handle_error(): 
+    try:
+        if number_of_value <= 1 :
+            raise ValueError
+        if not is_string_in_string(user_value, separator) :
+            raise ValueError
+    except (ValueError, IndexError):
+        quit_program()
+
+# Fonctions
+def is_string_in_string(first_value, second_value):
+    for i in range(len(first_value) - len(second_value) +1):
+            if second_value == first_value[i: i +len(second_value)]:
+                return True
+    return False
+
+def split_function(u_value,sep):
+    word =''
+    new_array = []
+    length = len(sep)   
+    for i in range(len(u_value)):
+            if sep == u_value[i: i+length] :
+                new_array.append(word)
+                word = ''
+                i += length
+                index_fin_sep=i
+            else:
+                word += u_value[i]
+    new_array.append(u_value[index_fin_sep:])
+    return new_array
+
+def quit_program():
+    sys.exit("error")
+
+# Résolution
+handle_error()
+result= split_function(user_value, separator)
+
+# Affichage Résultat
+for string in result:
+    print(string)
