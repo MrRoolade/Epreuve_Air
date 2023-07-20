908653b6f02cabfbf6e3f4a13463bb11b29a7eb2
MrRoolade <romain.recasens@free.fr>
2023-07-20 11:32:36 +0200

    Insertion dans un tableau trié - backup

---
+++ Air07.py
@@ -0,0 +1,39 @@
+#~~> MrRøølåÐe <~~#
+## Insertion dans un tableau trié
+### un programme qui ajoute à une liste d’entiers triée un nouvel entier tout en gardant la liste triée dans l’ordre croissant.
+#### Le dernier argument est l’élément à ajouter.
+##### Afficher error et quitter le programme en cas de problèmes d’arguments.
+
+import sys
+
+# Parsing
+user_value = sys.argv[1:-1]
+number_of_value = len(sys.argv)
+insert_value = sys.argv[-1]
+
+# Gestion des erreurs 
+def handle_error(): 
+    try:
+        if number_of_value <= 2 :
+            raise ValueError
+        for value in user_value:
+            if not value.isdigit():
+                raise ValueError
+    except (ValueError, IndexError):
+        quit_program()
+
+# Fonctions
+def insert_value_sorted(u_value, insert):
+    new_array = []
+             
+    return new_array
+
+def quit_program():
+    sys.exit("error")
+
+# Résolution
+handle_error()
+result= insert_value_sorted(user_value, insert_value)
+
+# Affichage Résultat
+print(" ".join(result))
