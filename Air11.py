f352ea8e1fafe0cbabdbf131160bd37948e1cd70
MrRoolade <romain.recasens@free.fr>
2023-07-26 14:45:32 +0200

    Afficher une pyramide

--- Air11.py
+++ Air11.py
@@ -3,7 +3,7 @@
 ### Afficher un escalier constitué d’un caractère et d’un nombre d’étages donné en paramètre.
 #### Afficher error et quitter le programme en cas de problèmes d’arguments.
 
-import sys ,os.path
+import sys
 
 # Parsing
 user_value = sys.argv[1:]
@@ -14,9 +14,9 @@ number_floor = user_value[1]
 def handle_error(): 
     try:
         if len(sys.argv) != 3 or not user_value[1].isdigit():
-            quit_program()
+            quit_program("error")
     except (ValueError, IndexError):
-        quit_program()
+        quit_program("error")
 
 # Fonctions
 def make_pyramide(motif, number): 
@@ -27,12 +27,11 @@ def make_pyramide(motif, number):
         pyramid=(space+block)
         print(pyramid)
 
-def quit_program():
-    sys.exit("error")   
+def quit_program(message):
+    sys.exit(message)   
 
 # Résolution
 handle_error()
-
 
 # Affichage Résultat
 make_pyramide(pattern , number_floor)
