291f22313383c85f90cc935fefbde9d642425396
MrRoolade <128980419+MrRoolade@users.noreply.github.com>
2023-10-03 22:18:01 +0200

    Le roi des tris

--- Air12.py
+++ Air12.py
@@ -7,32 +7,33 @@ import sys
 
 # Parsing
 user_array = sys.argv[1:]
-arr = []
 
 # Gestion des erreurs 
 def handle_error(): 
-    try:
-        if len(sys.argv) < 2 :
-            quit_program("error")
+    if len(sys.argv) < 2 :
+        quit_program("error")
+    try:        
+        user_array_int = [int(value) for value in user_array]
     except (ValueError, IndexError):
         quit_program("error")
 
 # Fonctions
-def quick_sort_array(arr):
-    if len(arr)<= 1:
-        return arr
-    pivot = arr[int(len(arr)/2)]
-    arr_low=[x for x in arr if x < pivot]
-    arr_high=[x for x in arr if x > pivot]
-    middle=[x for x in arr if x == pivot]
-    return quick_sort_array(arr_low) + middle + quick_sort_array(arr_high)
-
+def quick_sort_array(array):
+    if len(array)<= 1:
+        return array
+    pivot = int(array[len(array)//2])
+    array_low=[x for x in array if int(x)< pivot]
+    array_high=[x for x in array if int(x) > pivot]
+    middle=[x for x in array if int(x) == pivot]
+    return quick_sort_array(array_low) + middle + quick_sort_array(array_high)
     
 def quit_program(message):
     sys.exit(message)   
 
 # Résolution
 handle_error()
+result = quick_sort_array(user_array)
 
 # Affichage Résultat
-print(quick_sort_array(user_array))
+for i in result:
+    print(i, end=" ")
